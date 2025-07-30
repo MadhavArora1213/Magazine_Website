5fdba79261544b4dff229984d345f293ec79e8ab
Madhav Arora <madhavarora132005@gmail.com>
2025-07-30 23:39:21 +0530

    Update th App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+      <h1 className="text-8xl font-extrabold text-red-600 mb-6 animate-pulse">
+        Coming Soon
       </h1>
+    
     </div>
   )
 }
